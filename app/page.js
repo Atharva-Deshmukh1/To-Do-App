4f5d60d9c2d0b6f335a0c20844b7446428c47904
Atharva-Deshmukh1 <atharva25deshmukh@gmail.com>
2023-11-15 20:22:36 +0530

    update

--- app/page.js
+++ app/page.js
@@ -2,15 +2,16 @@
 import React, { useState } from 'react'
 const page = () => {
   const [title, settitle] = useState("");
-  const [desc, setdesc] = useState("")
+  // const [desc, setdesc] = useState("")
   const [maintask, setmaintask] = useState([])
 
 
   const submithandler = (e)=>{
     e.preventDefault()
-    setmaintask([...maintask, {title,desc}])
+    // setmaintask([...maintask, {title,desc}])
+    setmaintask([...maintask, {title}])
     settitle("")
-    setdesc("")
+    // setdesc("")
   }
 
   const deletehandler = (i)=>{
@@ -26,7 +27,7 @@ const page = () => {
         <li key={i}>
         <div className='main'>
         <h5> {t.title} </h5>
-        <h6> {t.desc} </h6>
+        {/* <h6> {t.desc} </h6> */}
         <button className='del' onClick = {()=>{deletehandler(i)}}
         >delete</button>
       </div>
@@ -37,6 +38,7 @@ const page = () => {
   return (
     <div>
       <>
+      
         <h1 className='heading'>To Do List</h1>
 
         <div className='form'>
@@ -50,12 +52,12 @@ const page = () => {
               }>
             </input>
 
-            <input placeholder='Enter Description'
+            {/* <input placeholder='Enter Description'
               value={desc}
               onChange={(e)=>
               setdesc(e.target.value)
               }>
-            </input>
+            </input> */}
 
             <button>Add Task</button>
 
